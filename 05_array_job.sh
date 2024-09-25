effe83c5d04a2c817b1d5035fdef64223a9bdfd0
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2024-09-25 16:06:11 +0200

    Update 05_array_job.sh

--- 05_array_job.sh
+++ 05_array_job.sh
@@ -3,14 +3,14 @@
 # usage: sbatch -a 1-100 05_array_job.sh
 
 # Array job shell script which runs R code
-# last edited 2024-09-04 by @vankesteren
+# last edited 2024-09-25 by @vankesteren
 # ODISSEI Social Data Science team
 
 # Set job requirements
 #SBATCH -n 1
 #SBATCH -t 00:15:00
 #SBATCH --ntasks 16
-#SBATCH -o ./logs/output.%a.out # STDOUT
+#SBATCH -o ./logs/output.%a.log # STDOUT
 
 # Loading modules
 module load 2023
